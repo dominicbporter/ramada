cdfa00fe32cc34d89d1113db66801d947dbee9ae
Unknown <dominic@dbp.kiwi>
2018-11-26 01:25:19 +1300

    Start of auckland page

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from web.views import index, auckland
+from web.views import index, auckland, albany, christchurch, orewa, queenstown, rotorua, taipa, wanaka
 
 urlpatterns = [
     path('', index.index_view, name='index'),
-    path('auckland/', auckland.auckland_view, name='auckland')
+    path('auckland/', auckland.page_view, name='auckland'),
+    path('albany/', albany.page_view, name='albany'),
+    path('christchurch/', christchurch.page_view, name='christchurch'),
+    path('orewa/', orewa.page_view, name='orewa'),
+    path('queenstown/', queenstown.page_view, name='queenstown'),
+    path('rotorua/', rotorua.page_view, name='rotorua'),
+    path('taipa/', taipa.page_view, name='taipa'),
+    path('wanaka/', wanaka.page_view, name='wanaka'),
 ]
--- web/views/auckland.py
+++ web/views/auckland.py
@@ -1,6 +1,5 @@
-from django.http import request
 from django.shortcuts import render
 
 
-def auckland_view(request):
+def page_view(request):
     return render(request, 'auckland.html', context={})
